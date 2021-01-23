7724ce54c305bea97f370ae9c4d2c4e47d878d6c
Chris Bohlman <cbohlmanaz@gmail.com>
2021-01-23 11:09:06 -0400

    added done and delete options to work items

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let submitBtns
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    submitBtns = document.querySelectorAll('.submit-btn')
+    const submitBtns = document.querySelectorAll('.submit-btn')
     submitBtns.forEach(btn => btn.addEventListener('click', addTask))
 });
 
@@ -15,7 +14,30 @@ function addTask(e) {
         return;
     }
     const li = document.createElement('li')
-    li.textContent = input.value
+    const div = document.createElement('div')
+    div.style.display='inline'
+
+    const p = document.createElement('p')
+    p.textContent = input.value
+    p.style.display = 'inline'
+
+    const btnCheck = document.createElement('span')
+    btnCheck.innerHTML = '<button id="check">✓</button>'
+    btnCheck.addEventListener('click', (e) => {
+        e.target.parentNode.parentNode.querySelector('p').classList.toggle('item-done')
+    })
+
+    const btnDelete = document.createElement('span')
+    btnDelete.innerHTML = '<button id="delete">✖</button>'
+    btnDelete.addEventListener('click', (e) => {
+        console.log(e.target.parentNode.parentNode)
+        e.target.parentNode.parentNode.parentNode.style.display = 'none'
+    })
+
+    div.appendChild(p)
+    div.appendChild(btnCheck)
+    div.appendChild(btnDelete)
+    li.appendChild(div)
 
     input.value = ''
     
