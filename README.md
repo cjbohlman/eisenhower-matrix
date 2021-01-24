479c13aeabb2525bfabb5d0ae3131c2810a09278
Chris Bohlman <cbohlmanaz@gmail.com>
2021-01-23 21:39:20 -0400

    Fixing CSS

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # eisenhower-matrix
 ## Open source Eisenhower matrix web app that does not need an account to register
 
- - [ ] Build the web app
- - [ ] Make it progressive
+ - [ ] Make buttons align to right of list item
+ - [ ] Add borders
+ - [ ] Add nice color scheme
+ - [ ] Fix square headings
+ - [ ] Fix input to look nicer
+ - [ ] support multi-line tasks
+ - [ ] Add character limit to input
+ - [ ] Add task sub-notes
+ - [ ] Add local/session storage support
  - [ ] Offer data import + export
--- script.js
+++ script.js
@@ -4,11 +4,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     submitBtns.forEach(btn => btn.addEventListener('click', addTask))
 });
 
-
-
 function addTask(e) {
     e.preventDefault()
-    const quadrantDiv = e.target.parentNode
+    const quadrantDiv = e.target.parentNode.parentNode
     const input = quadrantDiv.querySelector('input')
     if (input.value === '' ) {
         return;
@@ -20,17 +18,18 @@ function addTask(e) {
     const p = document.createElement('p')
     p.textContent = input.value
     p.style.display = 'inline'
+    p.style.width = '90%'
 
     const btnCheck = document.createElement('span')
     btnCheck.innerHTML = '<button id="check">✓</button>'
     btnCheck.addEventListener('click', (e) => {
-        e.target.parentNode.parentNode.querySelector('p').classList.toggle('item-done')
+        console.log(e.target)
+        e.target.parentNode.parentNode.parentNode.querySelector('p').classList.toggle('item-done')
     })
 
     const btnDelete = document.createElement('span')
     btnDelete.innerHTML = '<button id="delete">✖</button>'
     btnDelete.addEventListener('click', (e) => {
-        console.log(e.target.parentNode.parentNode)
         e.target.parentNode.parentNode.parentNode.style.display = 'none'
     })
 
