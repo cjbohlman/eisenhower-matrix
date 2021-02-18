d61179e72476496d0901cb36ac2fe0905d2efdbe
Chris Bohlman <cbohlmanaz@gmail.com>
2021-02-17 21:36:24 -0400

    More display fixes

--- script.js
+++ script.js
@@ -2,7 +2,29 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const submitBtns = document.querySelectorAll('.submit-btn')
     submitBtns.forEach(btn => btn.addEventListener('click', addTask))
+    
+    const hideInfoButton = document.querySelector('.info-btn')
+    hideInfoButton.addEventListener('click' , toggleInfo)
 });
+
+function toggleInfo(e) {
+    e.preventDefault()
+    const button = e.target
+    const downArrow = '\u02C5' //&#709;
+    const upArrow = '\u02C4' //&#708;
+    const info = document.querySelector('.info')
+    if (button.textContent == upArrow) {
+        // hide content
+        button.textContent = downArrow
+        info.classList.remove('disappear')
+    
+    }
+    else {
+        // show content
+        button.textContent = upArrow
+        info.classList.add('disappear')
+    }
+}
 
 function addTask(e) {
     e.preventDefault()
@@ -22,14 +44,14 @@ function addTask(e) {
     p.style.width = '90%'
 
     const btnCheck = document.createElement('span')
-    btnCheck.innerHTML = '<button id="check">✓</button>'
+    btnCheck.innerHTML = '<button class="chk-btn" id="check">✓</button>'
     btnCheck.addEventListener('click', (e) => {
         console.log(e.target)
         e.target.parentNode.parentNode.parentNode.querySelector('p').classList.toggle('item-done')
     })
 
     const btnDelete = document.createElement('span')
-    btnDelete.innerHTML = '<button id="delete">✖</button>'
+    btnDelete.innerHTML = '<button class="del-btn" id="delete">✖</button>'
     btnDelete.addEventListener('click', (e) => {
         e.target.parentNode.parentNode.parentNode.style.display = 'none'
     })
